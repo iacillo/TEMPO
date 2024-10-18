081026004e2ca28f9f9ff81c54297235e888d9d8
PROFESSOR IACILLO <77495829+iacillo@users.noreply.github.com>
2024-10-18 15:15:24 -0300

    Update script.js 001
    teste com https do IP

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const key = 'f065fa37f8d245f7b35221028240810'
 async function getLocalizacao(){
     //https://meuip.com/api/meuip.php
 
-    const base = `http://ip-api.com/json/`
+    const base = `https://ip-api.com/json/`
     //const base = `https://ipapi.co/json/`;
     const response = await fetch(base)
     const dados = await response.json()
